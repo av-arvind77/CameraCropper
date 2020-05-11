b38678b01598bc2a46fc79b886304fd37d0f2907
Arvind Pradhan <arvind.pradhan@artivatic.ai>
2020-05-11 20:25:34 +0530

    Refactor

--- cameracontrollersdk/src/main/java/com/artivatic/cameracontrollersdk/CameraControllerActivity.kt
+++ cameracontrollersdk/src/main/java/com/artivatic/cameracontrollersdk/CameraControllerActivity.kt
@@ -1,6 +1,6 @@
 package com.artivatic.cameracontrollersdk
 
-import android.Manifest
+import  android.Manifest
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
